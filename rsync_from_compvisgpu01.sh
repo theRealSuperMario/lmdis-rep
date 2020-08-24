2373877eb75b26e5fec5a3a143eff2f7f7c07c41
Sandro Braun <supermario94123@gmail.com>
2020-08-24 11:55:14 +0200

    adds rsync script

---
+++ rsync_from_compvisgpu01.sh
@@ -0,0 +1,2 @@
+rsync -av --progress -e ssh compvisgpu01:/export/home/sabraun/projects/gcpr_2020/lmdis-rep/results/pennaction_25/* results/pennaction_25/
+rsync -av --progress -e ssh compvisgpu01:/export/home/sabraun/projects/gcpr_2020/lmdis-rep/results/cub_25/* results/cub_25/
